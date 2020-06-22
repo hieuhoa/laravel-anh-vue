8a18a299bfe03db2a6a8524da4348aa4eeb73b06
hieuhoa <hieuhoa1408@gmail.com>
2020-06-22 14:39:48 +0700

    demo lv and vue

---
+++ resources/assets/js/components/UserDashboard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="user-dashboard">
+       <div class="list-user-comp">
+            <h4>List User</h4>
+            <list-user @userSelected="childrenSelectUser"></list-user>
+       </div>
+       <div class="user-detail-comp">
+            <h4>User Detail</h4>
+            <user-detail :userDetailInfo="userSelectedFromChild"></user-detail>
+       </div>
+    </div>
+</template>
+
+<script>
+    import ListUser from './ListUser.vue'
+    import UserDetail from './UserDetail.vue'
+    export default {
+        components:{
+            ListUser,
+            UserDetail
+        },
+        data(){
+            return {
+                userSelectedFromChild:{}
+            }
+        },
+        methods:{
+            childrenSelectUser(user){
+                //console.log(user)
+                this.userSelectedFromChild =user
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,18 @@
-<?php
-echo "chao em";
 
-?>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" value="{{ csrf_token() }}">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <user-dashboard></user-dashboard>
+        </div>
+
+         <script src="{{ mix('js/app.js') }}"></script> 
+        <!-- <script src="{{asset('js/app.js')}}"></script> -->
+    </body>
+</html>
